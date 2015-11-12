b0860953210477265d76a106a11531b7b1b1abe3
Querijn Heijmans <querijn@irule.at>
2015-11-12 00:38:59 -0800

    Added more renek skills and generalised movement

--- Game/RG2015/Assets/Scripts/Player.cs
+++ Game/RG2015/Assets/Scripts/Player.cs
@@ -5,8 +5,15 @@ using System;
 public class Player : MonoBehaviour
 {
     public float m_MovementSpeed = 5.0f;
+
+    public float m_DashSpeed = 1000.0f;
+    public float m_DashDistance = 0.0005f;
+
     private Vector3 m_WalkingDestination;
-    private float m_AnimationBlocking = 0.0f;
+
+    public String m_Character = "Renekton";
+    private bool m_AnimationBlocking = false;
+    private String m_AnimationPlaying;
 
     // Use this for initialization
     void Start ()
@@ -20,20 +27,62 @@ public class Player : MonoBehaviour
         UpdateKeyboard();
         UpdateMovement();
 
-        HandleAnimation();
+        if(m_AnimationBlocking)
+            m_AnimationBlocking = GetComponent<Animation>().IsPlaying(m_AnimationPlaying);
     }
 
     void UpdateKeyboard()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            PlayAnimation("RenektonQ", true);
+            PlayAnimation(m_Character + "Q", true);
+
+            switch (m_Character)
+            {
+                case "Renekton":
+                    break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PlayAnimation(m_Character + "W", true);
+
+            switch (m_Character)
+            {
+                case "Renekton":
+                    CancelMovement();
+                    break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            switch (m_Character)
+            {
+                case "Renekton":
+                    if (IsCasting())
+                        break;
+
+                    PlayAnimation(m_Character + "E", true);
+                    RaycastHit hit;
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000, LayerMask.GetMask("Floor")) && hit.transform)
+                    {
+                        Vector3 t_Direction = (hit.point - transform.position).normalized * m_DashDistance;
+
+                        m_WalkingDestination = transform.position + t_Direction;
+                    }
+
+
+                    break;
+            }
         }
     }
 
     void UpdateMovement()
     { 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1) && CanWalk())
         {
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000, LayerMask.GetMask("Floor")) && hit.transform)
@@ -47,32 +96,75 @@ public class Player : MonoBehaviour
         {
             t_Direction.y = 0;
             t_Direction = t_Direction.normalized;
-
-            transform.Translate(t_Direction * m_MovementSpeed * Time.deltaTime, Space.World);
+            
+            transform.Translate(t_Direction * GetSpeed() * Time.deltaTime, Space.World);
             transform.forward = t_Direction;
-            PlayAnimation("RenektonRun");
+            PlayAnimation(m_Character + "Run");
         }
-        else PlayAnimation("RenektonIdle");
+        else PlayAnimation(m_Character + "Idle");
+    }
+
+    void CancelMovement()
+    {
+        m_WalkingDestination = transform.position;
     }
 
     void PlayAnimation(String a_Animation, bool a_Block = false)
     {
-        if (m_AnimationBlocking <= 0)
+        if (!m_AnimationBlocking)
         {
-            if(!a_Block)
-                GetComponent<Animation>().CrossFade(a_Animation);
-            else GetComponent<Animation>().Play(a_Animation);
+            m_AnimationPlaying = a_Animation;
+            GetComponent<Animation>().CrossFade(a_Animation);
         }
-
-
-        if (a_Block && m_AnimationBlocking <= 0)
+        
+        if (a_Block && (!m_AnimationBlocking || !GetComponent<Animation>().IsPlaying(m_AnimationPlaying)))
         {
-            m_AnimationBlocking = GetComponent<Animation>().GetClip(a_Animation).length;
+            m_AnimationBlocking = true;
         }
     }
 
-    void HandleAnimation()
+    bool CanWalk()
     {
-        m_AnimationBlocking -= Time.deltaTime;
+        if (IsCasting('W'))
+            return false;
+
+        if (IsDashing())
+            return false;
+
+
+        return true;
+    }
+
+    bool IsDashing()
+    {
+        if(m_Character=="Renekton")
+        {
+            return IsCasting('E');
+        }
+
+        return false;
+    }
+
+    float GetSpeed()
+    {
+        if (IsDashing())
+            return m_DashSpeed;
+
+        return m_MovementSpeed;
+    }
+    
+    bool IsCasting(char Key = '0')
+    {
+        if(Key == '0')
+        {
+            if (GetComponent<Animation>().IsPlaying(m_Character + 'Q') ||
+                GetComponent<Animation>().IsPlaying(m_Character + 'R') ||
+                GetComponent<Animation>().IsPlaying(m_Character + 'E') ||
+                GetComponent<Animation>().IsPlaying(m_Character + 'W'))
+                return true;
+            else return false;
+        }
+
+        return GetComponent<Animation>().IsPlaying(m_Character + Key);
     }
 }
