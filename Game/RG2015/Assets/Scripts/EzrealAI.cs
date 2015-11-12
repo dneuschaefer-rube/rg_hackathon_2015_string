314e62e32f9b55091b4aab223a2d2cd0babbafa3
Querijn Heijmans <querijn@irule.at>
2015-11-12 13:19:32 -0800

    Added ezreal with AI

--- Game/RG2015/Assets/Scripts/EzrealAI.cs
+++ Game/RG2015/Assets/Scripts/EzrealAI.cs
@@ -2,34 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CaitlynAI : MonoBehaviour
+public class EzrealAI : MonoBehaviour
 {
-    public GameObject m_TrapPrefab = null;
+    public GameObject m_QShotPrefab = null;
 
     private bool m_AnimationBlocking = false;
     private string m_AnimationPlaying;
 
-    public float m_TrapPlacingChance = 20.0f;
+    public float m_QShotChance = 20.0f;
     public float m_RunningChance = 40.0f;
     public float m_IdleChance = 40.0f;
 
     public float m_MovementSpeed = 5f;
-    public uint m_MaxTraps = 15;
 
     public Vector3 m_Origin;
 
     private float m_IdleTimer = 0.0f;
 
     private Vector3 m_Destination;
-
-    private List<GameObject> m_OwnedTraps = new List<GameObject>();
-    
+        
     enum State
     {
         UNKNOWN,
         IDLING,
         RUNNING,
-        PLACING_TRAP
+        SHOOTING
     };
 
     State m_CurrentState = State.UNKNOWN;
@@ -70,10 +67,10 @@ public class CaitlynAI : MonoBehaviour
                 }
                 break;
 
-            case State.PLACING_TRAP:
-                if (!IsPlacingTrap())
+            case State.SHOOTING:
+                if (!IsShooting())
                 {
-                    PlayAnimation("CaitlynIdle");
+                    PlayAnimation("EzrealIdle");
                     goto case State.UNKNOWN;
                 }
                 break;
@@ -84,7 +81,7 @@ public class CaitlynAI : MonoBehaviour
     {
         float t_Roll = Random.Range(0F, 1.0F);
 
-        if((t_Roll -= m_TrapPlacingChance * 0.01f) < 0.0f)
+        if((t_Roll -= m_QShotChance * 0.01f) < 0.0f)
         {
             PlaceTrap();
         }
@@ -100,9 +97,9 @@ public class CaitlynAI : MonoBehaviour
 
     void PlaceTrap()
     {
-        //Debug.Log("Caitlyn is placing a trap.");
-        m_CurrentState = State.PLACING_TRAP;
-        PlayAnimation("CaitlynW", true);
+        //Debug.Log("Ezreal is shooting.");
+        m_CurrentState = State.SHOOTING;
+        PlayAnimation("EzrealQ", true);
 
 
         Bounds t_Floor = m_Floors[Random.Range(0, m_Floors.Length)].GetComponent<BoxCollider>().bounds;
@@ -110,29 +107,25 @@ public class CaitlynAI : MonoBehaviour
         float t_X = Random.Range(t_Floor.min.x, t_Floor.max.x);
         float t_Y = Random.Range(t_Floor.min.z, t_Floor.max.z);
 
-        Vector3 t_TrapDestination = new Vector3(t_X, 0, t_Y);
+        Vector3 t_ShotDestination = new Vector3(t_X, 0, t_Y);
 
-        transform.forward = (t_TrapDestination - transform.position).normalized;
-        m_OwnedTraps.Add((GameObject)GameObject.Instantiate(m_TrapPrefab, t_TrapDestination, Quaternion.identity));
-
-        if(m_OwnedTraps.Count > m_MaxTraps)
-        {
-            Destroy(m_OwnedTraps[0]);
-            m_OwnedTraps.Remove(m_OwnedTraps[0]);
-        }
+        transform.forward = (t_ShotDestination - transform.position).normalized;
+        GameObject.Instantiate(m_QShotPrefab, t_ShotDestination, Quaternion.identity);
+        
+        // trap prefab set destination
 
     }
 
-    bool IsPlacingTrap()
+    bool IsShooting()
     {
-        return GetComponent<Animation>().IsPlaying("CaitlynW");
+        return GetComponent<Animation>().IsPlaying("EzrealQ");
     }
 
     void Run()
     {
-        //Debug.Log("Caitlyn is running.");
+        //Debug.Log("Ezreal is running.");
         m_CurrentState = State.RUNNING;
-        PlayAnimation("CaitlynRun");
+        PlayAnimation("EzrealRun");
 
         m_Destination = transform.position;
         m_Destination.x = m_Origin.x + Random.Range(-10.0f, 10.0f);
@@ -141,9 +134,9 @@ public class CaitlynAI : MonoBehaviour
     void Idle()
     {
         m_IdleTimer = Random.Range(0.2f, 1.0f);
-        //Debug.Log("Caitlyn is idling for " + m_IdleTimer + " sec.");
+        //Debug.Log("Ezreal is idling for " + m_IdleTimer + " sec.");
         m_CurrentState = State.IDLING;
-        PlayAnimation("CaitlynIdle");
+        PlayAnimation("EzrealIdle");
     }
 
     void PlayAnimation(string a_Animation, bool a_Block = false)
