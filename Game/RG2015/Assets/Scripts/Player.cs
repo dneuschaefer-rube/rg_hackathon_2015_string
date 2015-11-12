00afe14a69dfe28343e51703facb9432cf1d7368
Querijn Heijmans <querijn@irule.at>
2015-11-11 23:33:11 -0800

    Added movement for player in a test environment

--- Game/RG2015/Assets/Scripts/Player.cs
+++ Game/RG2015/Assets/Scripts/Player.cs
@@ -4,26 +4,75 @@ using System;
 
 public class Player : MonoBehaviour
 {
-    public float MovementSpeed = 3.0f;
+    public float m_MovementSpeed = 5.0f;
+    private Vector3 m_WalkingDestination;
+    private float m_AnimationBlocking = 0.0f;
 
     // Use this for initialization
     void Start ()
     {
-        GetComponent<Animation>().Play("NidaleeRun");
+
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        float t_MovementHorizontal = Input.GetAxis("Horizontal") * MovementSpeed;
-        float t_MovementVertical = Input.GetAxis("Vertical") * MovementSpeed;
+        UpdateKeyboard();
+        UpdateMovement();
 
-        Vector3 t_Direction = new Vector3(t_MovementHorizontal * Time.deltaTime, 0.0f, t_MovementVertical * Time.deltaTime);
-        
-        transform.Translate(t_Direction, Space.World);
+        HandleAnimation();
+    }
 
-        // TODO FIX LOL 
-        //t_Direction.Normalize();
-        //transform.Rotate(new Vector3(0, 1, 0), Mathf.Atan2(t_Direction.y, t_Direction.x));
+    void UpdateKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayAnimation("RenektonQ", true);
+        }
+    }
+
+    void UpdateMovement()
+    { 
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000, LayerMask.GetMask("Floor")) && hit.transform)
+            {
+                m_WalkingDestination = hit.point;
+            }
+        }
+
+        Vector3 t_Direction = m_WalkingDestination - transform.position;
+        if (t_Direction.sqrMagnitude > 0.01f)
+        {
+            t_Direction.y = 0;
+            t_Direction = t_Direction.normalized;
+
+            transform.Translate(t_Direction * m_MovementSpeed * Time.deltaTime, Space.World);
+            transform.forward = t_Direction;
+            PlayAnimation("RenektonRun");
+        }
+        else PlayAnimation("RenektonIdle");
+    }
+
+    void PlayAnimation(String a_Animation, bool a_Block = false)
+    {
+        if (m_AnimationBlocking <= 0)
+        {
+            if(!a_Block)
+                GetComponent<Animation>().CrossFade(a_Animation);
+            else GetComponent<Animation>().Play(a_Animation);
+        }
+
+
+        if (a_Block && m_AnimationBlocking <= 0)
+        {
+            m_AnimationBlocking = GetComponent<Animation>().GetClip(a_Animation).length;
+        }
+    }
+
+    void HandleAnimation()
+    {
+        m_AnimationBlocking -= Time.deltaTime;
     }
 }
