972a1052a1d4b207fe7b2cfa88dfbcb73c98cc8b
MTGUli <gametester03@yahoo.com>
2015-11-13 06:10:27 +0000

    LuxStart

---
+++ Game/RG2015/Assets/Scripts/LuxLaser.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LuxLaser : MonoBehaviour {
+
+    public int damage = 400;
+    public GameObject redLaser;
+    public GameObject rainbowLaser;
+
+    public float redLaserTime = 0.1f;
+    public float rainbowLaserTime = 0.25f;
+
+    public float redLaserWidthStart = 0.05f;
+    public float redLaserWidthEnd = 0.1f;
+
+    private float startTime;
+    private float journeyLength;
+
+    enum State
+    {
+        UNKNOWN,
+        WARNING,
+        SHOOT,
+        DESTROY
+    };
+
+    State currentState = State.UNKNOWN;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnLuxLaser());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (currentState)
+        {
+            case State.WARNING:
+                float compl = (Time.time - startTime) / redLaserTime;
+                LineRenderer line = redLaser.GetComponent<LineRenderer>();
+                float lineWidth = Mathf.Lerp(redLaserWidthStart, redLaserWidthEnd, compl);
+                line.SetWidth(lineWidth, lineWidth);
+                break;
+            case State.SHOOT:
+                break;
+            case State.DESTROY:
+                Destroy(this.gameObject);
+                break;
+            default:
+                break;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Player>().ChangeHealth(-damage);
+        }
+    }
+
+    IEnumerator SpawnLuxLaser()
+    {
+        startTime = Time.time;
+        currentState = State.WARNING;
+        redLaser.SetActive(true);
+        yield return new WaitForSeconds(redLaserTime);
+        startTime = Time.time;
+        redLaser.SetActive(false);
+        rainbowLaser.SetActive(true);
+        currentState = State.SHOOT;
+        yield return new WaitForSeconds(rainbowLaserTime);
+        rainbowLaser.SetActive(false);
+        currentState = State.DESTROY;
+    }
+}
