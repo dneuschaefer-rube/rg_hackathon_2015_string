928ceb0efd10154378fb728f3c9d16d94a29542c
MTGUli <gametester03@yahoo.com>
2015-11-13 14:30:54 +0000

    .

--- Game/RG2015/Assets/Scripts/SpawnManager.cs
+++ Game/RG2015/Assets/Scripts/SpawnManager.cs
@@ -4,13 +4,36 @@ using System.Collections;
 public class SpawnManager : MonoBehaviour {
 
     GameObject globalScript;
+
+    public GameObject Ezreal;
+    public GameObject Caitlyn;
+    public GameObject Lux;
+    public GameObject Xerath;
+
+    public Vector3 SpawnPoint1;
+    public Vector3 SpawnPoint2;
+    public Vector3 LuxSpawnPoint;
+
+    public float[] spawnTimes;
 	// Use this for initialization
 	void Start () {
-        globalScript = GameObject.FindGameObjectWithTag("Global");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+    IEnumerator SpawnWaves()
+    {
+        for (int i = 0; i < spawnTimes.Length; i++)
+        {
+            yield return new WaitForSeconds(spawnTimes[i]);
+            if(i == 0)
+            {
+
+            }
+        }
+    }
 }
