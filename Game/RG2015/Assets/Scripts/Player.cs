8bd7f5af5c9104226f4d92c3c076c3316dcb37c7
MTGUli <gametester03@yahoo.com>
2015-11-13 00:43:44 +0000

    Smooth Camera Follow

--- Game/RG2015/Assets/Scripts/Player.cs
+++ Game/RG2015/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using System;
 
 public class Player : MonoBehaviour
 {
+    public GameObject camera;
+
     public float m_MovementSpeed = 5.0f;
 
     public float m_DashSpeed = 15.0f;
@@ -44,6 +46,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Update Camera X:
+        camera.transform.position = Vector3.Lerp(
+        camera.transform.position, new Vector3(this.transform.position.x, camera.transform.position.y, camera.transform.position.z),
+        Time.deltaTime);
+
         UpdateKeyboard();
         UpdateMovement();
 
