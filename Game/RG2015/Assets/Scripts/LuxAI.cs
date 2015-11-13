466d36eea4616a60b9bc998ae257cfa8bb3368d0
MTGUli <gametester03@yahoo.com>
2015-11-13 09:12:49 +0000

    .

--- Game/RG2015/Assets/Scripts/LuxAI.cs
+++ Game/RG2015/Assets/Scripts/LuxAI.cs
@@ -2,6 +2,14 @@
 using System.Collections;
 
 public class LuxAI : MonoBehaviour {
+    public GameObject m_LuxLaserPrefab = null;
+
+    private bool m_AnimationBlocking = false;
+    private string m_AnimationPlaying;
+    public float m_MovementSpeed = 5f;
+
+    public Vector3 m_Origin;
+    public Vector3 m_Destination;
 
 	// Use this for initialization
 	void Start () {
